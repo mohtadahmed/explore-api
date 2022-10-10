d1e17a0cb39e53200671c942b4fa470ea98ba3d3
mohtadahmed <mohtadsaeem13@gmail.com>
2022-10-11 01:31:21 +0600

    fetching data from json placeholder

---
+++ js/app.js
@@ -0,0 +1,15 @@
+// fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res => res.json())
+//     .then(json => console.log(json))
+
+
+function loadUsers(){
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(res => res.json())
+        .then(data => displayUsers(data))
+}
+
+
+function displayUsers(data){
+    console.log(data)
+}
